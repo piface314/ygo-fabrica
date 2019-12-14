ec6d96bf41b3ebda0ede0568e4f1891b19df6e3d
Henrique Santana <henriquessantana314@gmail.com>
2019-12-13 22:41:04 -0300

    Reestructuring the project

--- lua-modules/set-paths.lua
+++
@@ -1,3 +0,0 @@
-local version = _VERSION:match("%d+%.%d+")
-package.path = 'lua-modules/share/lua/' .. version .. '/?.lua;lua-modules/share/lua/' .. version .. '/?/init.lua;' .. package.path
-package.cpath = 'lua-modules/lib/lua/' .. version .. '/?.so;' .. package.cpath
