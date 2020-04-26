fedafc3df3019aecb4e9ce3770db581f273899c4
Henrique Santana <henriquessantana314@gmail.com>
2020-04-25 23:10:00 -0300

    Fixing TypeWriter text fitting

--- scripts/composer/type-writer.lua
+++ scripts/composer/type-writer.lua
@@ -5,6 +5,10 @@ local TypeWriter = {}
 
 local DPI = 300
 
+local function is_empty(text)
+  return text:match("^%s*$")
+end
+
 local function paint_insert(t, base, color, x, y)
   local tw, th = t:width(), t:height()
   if not color then color = { 0, 0, 0 } end
@@ -16,6 +20,7 @@ local function paint_insert(t, base, color, x, y)
 end
 
 function TypeWriter.print(text, base, color, args)
+  if is_empty(text) then return base end 
   local x, y, w, h, f, ff, a = args.x, args.y, args.w, args.h, args.f, args.ff, args.a
   local t = vips.Image.text(text, { font = f, fontfile = ff, dpi = DPI })
   if h then
@@ -24,7 +29,7 @@ function TypeWriter.print(text, base, color, args)
   if w and t:width() > w then
     t = t:resize(w / t:width(), { vscale = 1 })
   end
-  local tw, th = t:width(), t:height()
+  local tw = t:width()
   x, y = x or 0, y or 0
   if a == 'high' then
     x = x - tw
@@ -34,23 +39,60 @@ function TypeWriter.print(text, base, color, args)
   return paint_insert(t, base, color, x, y)
 end
 
-function TypeWriter.printf(text, base, color, args)
-  local x, y, w, h, ft, fs, ff, a, j, i = args.x, args.y, args.w, args.h, args.ft,
-    args.fs, args.ff, args.a or 'low', args.j, args.i or 16
-  local opt, t = { width = w, fontfile = ff, dpi = DPI, justify = j, align = a }
+local function fit_in_ratio(text, opt, h, ft, fs)
+  local t
   for _, size in ipairs(fs) do
     opt.font = ("%s %s"):format(ft, size)
     t = vips.Image.text(text, opt)
     if t:height() <= h then
-      return paint_insert(t, base, color, x, y)
+      return t
     end
   end
+  return nil
+end
+
+local function fit_relaxing_width(text, opt, h, w, i)
+  local scale, t = 1
+  local initial_wd = opt.width
   repeat
     opt.width = opt.width + i
+    scale = w / opt.width
+    if scale <= 0.5 then
+      opt.width = initial_wd
+      return nil
+    end
     t = vips.Image.text(text, opt)
   until t:height() <= h
-  local scale = w / opt.width
-  return paint_insert(t:resize(scale, { vscale = 1 }), base, color, x, y)
+  return t:resize(scale, { vscale = 1 })
+end
+
+local function fit_relaxing_width_and_font(text, opt, h, w, i, ft, fs)
+  local r, scale, t = 1, 1
+  local relax = {
+    function() fs = fs - 0.2 end,
+    function() opt.width = opt.width + i end
+  }
+  repeat
+    relax[r]()
+    scale = w / opt.width
+    if scale <= 0.5 then
+      return nil
+    end
+    opt.font = ("%s %s"):format(ft, fs)
+    r, t = 3 - r, vips.Image.text(text, opt)
+  until t:height() <= h
+  return t:resize(scale, { vscale = 1 })
+end
+
+function TypeWriter.printf(text, base, color, args)
+  if is_empty(text) then return base end 
+  local x, y, w, h, ft, fs, ff, a, j, i = args.x, args.y, args.w, args.h, args.ft,
+    args.fs, args.ff, args.a or 'low', args.j, args.i or 16
+  local opt = { width = w, fontfile = ff, dpi = DPI, justify = j, align = a }
+  local t = fit_in_ratio(text, opt, h, ft, fs)
+    or fit_relaxing_width(text, opt, h, w, i)
+    or fit_relaxing_width_and_font(text, opt, h, w, i, ft, fs[#fs])
+  return t and paint_insert(t, base, color, x, y) or base
 end
 
 return TypeWriter
