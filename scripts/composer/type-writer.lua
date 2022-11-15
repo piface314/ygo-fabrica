0c1d1d9422851304559f1184dbb219308a9b22b4
Henrique Santana <henriquessantana314@gmail.com>
2022-11-15 11:48:16 -0300

    Improved text formatting

--- scripts/composer/type-writer.lua
+++ scripts/composer/type-writer.lua
@@ -1,4 +1,5 @@
 local vips = require 'vips'
+local fun = require 'lib.fun'
 
 --- Provides text printing functions.
 local TypeWriter = {}
@@ -29,9 +30,13 @@ local function expand_markup(text)
   return text:gsub('%b<>', function(s)
     if s == '</>' then return '</span>' end
     return (s:gsub('^<', '<span '):gsub('(.)=(%-?%d+%.?%d*)', apply_markup))
-  end)
+  end):gsub('● ', '●\xC2\xA0')
 end
 
+---Ensures `text` is a string with at least one non-blank character, otherwise
+---returns `nil`.
+---@param text string|nil
+---@return string|nil
 local function prepare_text(text)
   if not text then return nil end
   if type(text) ~= 'string' then text = tostring(text) end
@@ -59,19 +64,25 @@ local function fit_in_ratio(text, opt, h, ft, fs)
   return nil
 end
 
-local function fit_relaxing_width(text, opt, h, w, i)
+local function fit_relaxing_width(text, opt, h, w, i, ft, fs)
   local scale, t = 1, nil
   local initial_wd = opt.width
-  repeat
-    opt.width = opt.width + i
-    scale = w / opt.width
-    if scale <= 0.5 then
-      opt.width = initial_wd
-      return nil
+  for _, size in ipairs(fs) do
+    opt.font = ('%s %s'):format(ft, size)
+    repeat
+      opt.width = opt.width + i
+      scale = w / opt.width
+      if scale <= 0.75 then
+        opt.width = initial_wd
+        break
+      end
+      t = vips.Image.text(text, opt)
+    until t:height() <= h
+    if scale > 0.75 then
+      return t:resize(scale, {vscale = 1})
     end
-    t = vips.Image.text(text, opt)
-  until t:height() <= h
-  return t:resize(scale, {vscale = 1})
+  end
+  return nil
 end
 
 local function fit_relaxing_width_and_font(text, opt, h, w, i, ft, fs)
@@ -90,6 +101,10 @@ local function fit_relaxing_width_and_font(text, opt, h, w, i, ft, fs)
   return t:resize(scale, {vscale = 1})
 end
 
+local function gap_size(t, h)
+  return t and h - t:height() or nil
+end
+
 local align = {left = 'low', center = 'centre', right = 'high'}
 --- Prints formatted `text` in an area to a background image (`base`).
 --- This function supports word wrapping and tries to fit the text in
@@ -97,10 +112,10 @@ local align = {left = 'low', center = 'centre', right = 'high'}
 --- `args` specify multiple layout parameters.
 --- `color` must be a string in hex format (e.g. '#ffffff' -> white).
 --- If none is specified, black ('#000000') is used to print that text.
---- @param text string
+--- @param text string|nil
 --- @param base Image
 --- @param args table
---- @param color? string
+--- @param color? string|nil
 --- @return any
 function TypeWriter.printf(text, base, args, color)
   text = prepare_text(text)
@@ -110,17 +125,24 @@ function TypeWriter.printf(text, base, args, color)
   local x, y, w, h, ft, fs = args.x, args.y, args.w, args.h, args.ft, args.fs
   local ff, a, j, i = args.ff, align[args.a] or 'low', args.j, args.i or 16
   local opt = {width = w, fontfile = ff, dpi = DPI, justify = j, align = a}
-  local t = fit_in_ratio(text, opt, h, ft, fs)
-    or fit_relaxing_width(text, opt, h, w, i)
-    or fit_relaxing_width_and_font(text, opt, h, w, i, ft, fs[#fs])
-  return t and paint_insert(t, base, color, x, y) or base
+  local t = {
+    fit_in_ratio(text, opt, h, ft, fs),
+    fit_relaxing_width(text, opt, h, w, i, ft, fs),
+    fit_relaxing_width_and_font(text, opt, h, w, i, ft, fs[#fs])
+  }
+  local g = fun.range(1, 3):map(function(k) return k, gap_size(t[k], h) end):tomap()
+  local kmin = nil
+  for k = 1, 3 do
+    if g[k] and (not kmin or g[k] < g[kmin]) then kmin = k end
+  end
+  return t[kmin] and paint_insert(t[kmin], base, color, x, y) or base
 end
 
 --- Prints `text` in a single line to a background image (`base`).
 --- `args` specify multiple layout parameters.
 --- `color` must be a string in hex format (e.g. '#ffffff' -> white).
 --- If none is specified, black ('#000000') is used to print that text.
---- @param text string
+--- @param text string|nil
 --- @param base Image
 --- @param args table
 --- @param color? string
